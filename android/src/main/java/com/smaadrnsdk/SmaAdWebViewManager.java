2aa6ddb6b98c882dccab034968777129c029640a
gmoco-kawamura <158865565+gmoco-kawamura@users.noreply.github.com>
2024-04-23 18:08:38 +0900

    fix: fix java

--- android/src/main/java/com/smaadrnsdk/SmaAdWebViewManager.java
+++ android/src/main/java/com/smaadrnsdk/SmaAdWebViewManager.java
@@ -116,7 +116,7 @@ public class SmaAdWebViewManager extends SimpleViewManager<SmaAdWebView> {
     };
 
     // JavaScriptインターフェースを追加
-    webView.addJavascriptInterface(new JavaScriptBridgeInterface(listener, context), "Android");
+    webView.addJavascriptInterface(new JavaScriptBridgeInterface(context, listener), "Android");
 
     webView.setListener(activity, listener);
     updateWebView();
